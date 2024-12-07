18885ba35c486c2d1effd33fa0caa9b6c6605fbb
Sartkis_13 <petrosyants.s@yandex.ru>
2024-12-07 11:24:41 +0300

    Что то сделал

---
+++ SF HomeWork/SF HomeWork/bin/Debug/net8.0/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SF_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @" C:\Users\фвьшт\source\repos\SF HomeWork\SF HomeWork\Folder\";
+            var span = TimeSpan.FromMinutes(30);
+            var rootDirectory = new DirectoryInfo(dirName);
+
+            if (!Directory.Exists(dirName)) throw new Exception("Папака не существует");
+
+            Console.WriteLine("Folders");
+            var dirs = Directory.GetDirectories(dirName);
+
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine(dir);
+                Console.WriteLine();
+
+
+            }
+
+        }
+    }
+}
