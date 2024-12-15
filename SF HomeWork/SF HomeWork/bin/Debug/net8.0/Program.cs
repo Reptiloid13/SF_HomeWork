f86c5957622574007e2a218a607096c90bdaab6f
Sartkis_13 <petrosyants.s@yandex.ru>
2024-12-15 18:30:04 +0300

    1

--- SF HomeWork/SF HomeWork/bin/Debug/net8.0/Program.cs
+++ SF HomeWork/SF HomeWork/bin/Debug/net8.0/Program.cs
@@ -4,23 +4,69 @@
     {
         static void Main(string[] args)
         {
-            string dirName = @" C:\Users\фвьшт\source\repos\SF HomeWork\SF HomeWork\Folder\";
+            DirectoryInfo folder = new DirectoryInfo($@"{Environment.CurrentDirectory}");
             var span = TimeSpan.FromMinutes(30);
-            var rootDirectory = new DirectoryInfo(dirName);
 
-            if (!Directory.Exists(dirName)) throw new Exception("Папака не существует");
+            if (!folder.Exists) throw new Exception("Folder don't exist");
 
+            long totalFolderSize = folderSize(folder);
             Console.WriteLine("Folders");
-            var dirs = Directory.GetDirectories(dirName);
-
+            var dirs = folder.GetDirectories();
             foreach (var dir in dirs)
             {
                 Console.WriteLine(dir);
                 Console.WriteLine();
-
-
             }
 
+            Console.WriteLine($"Total  folder size in bytes: {totalFolderSize}");
+
         }
+        static long folderSize(DirectoryInfo folder)
+        {
+            long totalSizeOfDir = 0;
+            FileInfo[] allFiles = folder.GetFiles();
+            foreach (FileInfo file in allFiles)
+            {
+                totalSizeOfDir += file.Length;
+            }
+            DirectoryInfo[] subFolders = folder.GetDirectories();
+            foreach (DirectoryInfo dir in subFolders)
+            {
+                totalSizeOfDir += folderSize(dir);
+            }
+            return totalSizeOfDir;
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //if (!Directory.Exists(dirName)) throw new Exception("Папака не существует");
+
+        //Console.WriteLine("Folders");
+        //var dirs = Directory.GetDirectories(dirName);
+
+        //foreach (var dir in dirs)
+        //{
+        //    Console.WriteLine(dir);
+        //    Console.WriteLine();
+
+        //    //if(Directory.GetLastWriteTime(dir)< DateTime.Now.AddSeconds(-1))
+        //    //   {
+        //    //       Directory.Delete(dir, true); 
+        //    //   }
+        //}
+
+
+        //string[] files = Directory.GetFiles(dirName);
+        //Console.WriteLine(string.Join(", ", files));
     }
+
+
 }
---
+++ WorkWithExceptions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWithExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
